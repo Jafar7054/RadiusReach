c13c8fbe74634f9d0c57c7edf11c5e1e84b3a466
Jafar7054 <119682032+Jafar7054@users.noreply.github.com>
2025-03-13 04:15:59 +0000

    commit

--- radiusReach/src/main/java/com/radiusReach/App.java
+++ radiusReach/src/main/java/com/radiusReach/App.java
@@ -1,6 +1,7 @@
 package com.radiusReach;
 
-import org.springframework.boot.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Hello world!
  *
